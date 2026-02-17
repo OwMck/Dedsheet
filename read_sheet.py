ae763401d29530a3dba09cadf914007eb6497e0f
Owen <owen@example.com>
2026-02-17 03:47:24 +0000

    Flask tutorial setup (WIP)

--- read_sheet.py
+++ read_sheet.py
@@ -1,43 +1,26 @@
 import pandas as pd
-
+from distutils.log import debug
+from fileinput import filename
+from flask import Flask, request, render_template
+print(render_template)
+app = Flask(__name__) 
 
 # Reads excel sheet into a pandas dataframe and head shows top of spreadsheet
 #df = pd.read_excel("sales_data.xlsx", engine="openpyxl")
 #print(df.head())
 
+# https://www.geeksforgeeks.org/python/how-to-upload-file-in-python-flask/
+@app.route('/')  
+def main():  
+    return render_template("home.html")  
 
-from flask import Flask, request, render_template
-import pandas as pd
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        f.save(f.filename)  
+        return render_template("acknowledge.html", name = f.filename)  
 
-app = Flask(__name__)
+if __name__ == '__main__':  
+    app.run(debug=True)  
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    df_preview = None
-
-    if request.method == 'POST':
-        # Check if file was uploaded
-        if 'spreadsheet' not in request.files:
-            return "No file part"
-
-        file = request.files['spreadsheet']
-
-        if file.filename == '':
-            return "No selected file"
-
-        # Detect file type and read into pandas
-        ext = file.filename.split('.')[-1].lower()
-        if ext in ['xls', 'xlsx']:
-            df = pd.read_excel(file)
-        elif ext == 'csv':
-            df = pd.read_csv(file)
-        else:
-            return "Unsupported file type"
-
-        # Prepare a preview of the first few rows
-        df_preview = df.head().to_html()
-
-    return render_template('/website/home.html', df_preview=df_preview)
-
-if __name__ == "__main__":
-    app.run(debug=True)
